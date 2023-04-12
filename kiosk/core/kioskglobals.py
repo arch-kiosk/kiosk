38b95947ee794aa48e9e2741772475daa162398b
urapadmin <38838314+urapadmin@users.noreply.github.com>
2023-04-12 15:35:22 -0400

    supports filemaker template 14.2 and the new reduced config

--- kiosk/core/kioskglobals.py
+++ kiosk/core/kioskglobals.py
@@ -18,9 +18,9 @@ from uic.uicstream import UICStream, UICKioskFile
 import datetime
 import threading
 
-kiosk_version = "1.4.6"
+kiosk_version = "1.4.7"
 kiosk_version_name = "queasy queries"
-kiosk_date = datetime.datetime(2023, 4, 10)
+kiosk_date = datetime.datetime(2023, 4, 12)
 debug = False
 development = {}
 
