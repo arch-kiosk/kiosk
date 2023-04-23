a8fa9fb98bae773b069af79bf6cb4e847fda1730
urapadmin <38838314+urapadmin@users.noreply.github.com>
2023-04-23 13:46:47 -0400

    bumped up version

--- kiosk/core/kioskglobals.py
+++ kiosk/core/kioskglobals.py
@@ -18,9 +18,9 @@ from uic.uicstream import UICStream, UICKioskFile
 import datetime
 import threading
 
-kiosk_version = "1.4.8"
+kiosk_version = "1.4.9"
 kiosk_version_name = "queasy queries"
-kiosk_date = datetime.datetime(2023, 4, 18)
+kiosk_date = datetime.datetime(2023, 4, 22)
 debug = False
 development = {}
 
