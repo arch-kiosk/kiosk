c6570e353fc31b92aba705da4a16c6a5c8867d7c
urapadmin <38838314+urapadmin@users.noreply.github.com>
2023-06-28 23:48:36 +0200

    first version of transfer_kiosk_cli

---
+++ kiosk/core/transferkiosk.py
@@ -0,0 +1,312 @@
+import json
+import logging
+import datetime
+import os
+import pathlib
+from zipfile import ZipFile
+
+import kioskstdlib
+from filerepository import FileRepository
+from kioskconfig import KioskConfig
+from dsd.dsd3singleton import Dsd3Singleton
+from dsd.dsdview import DSDView
+from dsd.dsdyamlloader import DSDYamlLoader
+from kioskfilesmodel import KioskFilesModel
+from kiosksqldb import KioskSQLDb
+from databasedrivers import DatabaseDriver
+
+
+class TransferKiosk:
+
+    def __init__(self, cfg: KioskConfig):
+        self.cfg = cfg
+        self.enable_console = False
+        self.master_view = self._init_dsd(cfg)
+        self.console_log = print
+        self.temp_table_name = ""
+        self._delta = None
+
+    def setup_console(self):
+        if self.enable_console:
+            self.console_log = print
+        else:
+            self.console_log = lambda *args, **kwargs: None
+
+    @staticmethod
+    def _init_dsd(cfg):
+        master_dsd = Dsd3Singleton.get_dsd3()
+        master_dsd.register_loader("yml", DSDYamlLoader)
+        if not master_dsd.append_file(cfg.get_dsdfile()):
+            logging.error(f"TransferKiosk._init_dsd: dsd3 initialized: "
+                          f"{cfg.get_dsdfile()} could not be loaded by append_file.")
+            raise Exception(f"init_dsd: {cfg.get_dsdfile()} could not be loaded.")
+
+        try:
+            master_view = DSDView(master_dsd)
+            master_view_instructions = DSDYamlLoader().read_view_file(cfg.get_master_view())
+            master_view.apply_view_instructions(master_view_instructions)
+            logging.debug(f"TransferKiosk._init_dsd: dsd3 initialized: {cfg.get_dsdfile()}.")
+            return master_view
+        except BaseException as e:
+            logging.error("TransferKiosk._init_dsd: dsd3 initialized: "
+                          "Exception when applying master view to dsd: {repr(e)}")
+            raise e
+
+    def cat(self, target_dir: str) -> bool:
+        def json_serial(obj):
+            """JSON serializer for objects not serializable by default json code"""
+
+            if isinstance(obj, (datetime.datetime, datetime.date)):
+                return obj.isoformat()
+            raise TypeError("Type %s not serializable" % type(obj))
+
+        self.setup_console()
+        self.console_log(f"cataloguing ...")
+        if not self.master_view:
+            logging.error("Master View not initialized!")
+            return False
+
+        files_table = self.master_view.dsd.files_table
+        if not files_table:
+            logging.error("Not clear what the table for files is named in this kiosk.")
+            return False
+
+        self.console_log(f"image table is {files_table}")
+
+        if not os.path.exists(target_dir):
+            os.mkdir(target_dir)
+        cat_file = os.path.join(target_dir, 'cat_file.json')
+        self.console_log(f"Dumping file information into {cat_file}", end="")
+        cur = KioskSQLDb.execute_return_cursor(f"select uid, modified, md5_hash "
+                                               f"from images where not tags ilike %s", ["%BROKEN_FILE%"])
+        count = 0
+        with open(cat_file, 'w', encoding='utf-8') as f:
+            f.write("[\n")
+            while True:
+                data = cur.fetchmany(100)
+                if not data:
+                    break
+                for d in data:
+                    if count > 0:
+                        f.write(",\n")
+                    json.dump(d, f, default=json_serial)
+                    count += 1
+                    if count % 100 == 0:
+                        self.console_log(".", end="")
+
+            f.write("]\n")
+
+        self.console_log(f" okay: {count} file records dumped.")
+        return True
+
+    def create_files_delta(self, target_catalog, source_catalog):
+        if not (target_catalog or source_catalog):
+            logging.error(f"{self.__class__.__name__}.create_files_delta: "
+                          f"Called without either a target catalog or a source catalog.")
+            return False
+
+        if target_catalog and source_catalog:
+            logging.error(f"{self.__class__.__name__}.create_files_delta: "
+                          f"Called with BOTH a target catalog and a source catalog.")
+            return False
+
+        if target_catalog and not os.path.isfile(target_catalog):
+            logging.error(f"{self.__class__.__name__}.create_files_delta: "
+                          f"Target catalog {target_catalog} does not exist")
+            return False
+
+        if source_catalog and not os.path.isfile(source_catalog):
+            logging.error(f"{self.__class__.__name__}.create_files_delta: "
+                          f"Source catalog {source_catalog} does not exist")
+            return False
+
+        self.console_log(f"Importing {source_catalog if source_catalog else target_catalog} ", end="")
+        self._import_json_catalog(source_catalog if source_catalog else target_catalog)
+
+        c = KioskSQLDb.get_field_value_from_sql("c",
+                                                f"select count(uid) c from "
+                                                f"{KioskSQLDb.sql_safe_ident(self.temp_table_name)}")
+        recent = KioskSQLDb.get_field_value_from_sql("recent",
+                                                     f"select max(modified) recent from "
+                                                     f"{KioskSQLDb.sql_safe_ident(self.temp_table_name)}")
+        if c and recent:
+            self.console_log(f"... okay: {c} records found in catalog "
+                             f"of which the most recent is of {recent.isoformat()} ")
+            logging.debug(f"{self.__class__.__name__}._import_json_catalog: {c} records in temp table. "
+                          f"Most recent date is {recent.isoformat()}")
+        else:
+            self.console_log(f"... okay: {c} records found in catalog.")
+            logging.debug(f"{self.__class__.__name__}._import_json_catalog: {c} records in temp table. ")
+
+        files_table_sql = f"(select uid, modified, md5_hash from " \
+                          f"{KioskSQLDb.sql_safe_ident(self.master_view.dsd.files_table)} " \
+                          f"where not tags ilike " \
+                          f"{DatabaseDriver.quote_value('VARCHAR', DatabaseDriver.wildcard('BROKEN_FILE'))})"
+        if source_catalog:
+            self.console_log(f"Creating delta to transfer files from a remote Kiosk to this one ", end="")
+            delta = self._process_delta(KioskSQLDb.sql_safe_ident(self.temp_table_name), files_table_sql)
+        else:
+            self.console_log(f"Creating delta to transfer files from this Kiosk to a remote Kiosk ", end="")
+            delta = self._process_delta(files_table_sql, KioskSQLDb.sql_safe_ident(self.temp_table_name))
+
+        self.console_log(f"okay: {len(delta)} files need to be copied.")
+        self._delta = delta
+        return True
+
+    def delta2file(self, filename: str):
+        with open(filename, "w") as f:
+            json.dump(self._delta, f)
+
+    def file2delta(self, filename: str):
+        with open(filename, "r") as f:
+            self._delta = json.load(f)
+
+    def _import_json_catalog(self, catalog_file):
+        with open(catalog_file, "r") as f:
+            file_records = json.load(f)
+        self.temp_table_name = str(kioskstdlib.uuid4())
+        sql = f"CREATE TEMP TABLE IF NOT EXISTS {KioskSQLDb.sql_safe_ident(self.temp_table_name)} ("
+        sql += f"uid UUID UNIQUE PRIMARY KEY NOT NULL, " \
+               f"modified TIMESTAMP NOT NULL," \
+               f"md5_hash VARCHAR)"
+        KioskSQLDb.execute(sql)
+        sql_insert = f"INSERT INTO {KioskSQLDb.sql_safe_ident(self.temp_table_name)} VALUES(%s,%s,%s)"
+        for file in file_records:
+            KioskSQLDb.execute(sql_insert, [file[0], datetime.datetime.fromisoformat(file[1]), file[2]])
+
+    def _process_delta(self, from_kiosk, to_kiosk):
+        """
+        processes the delta. from_kiosk and to_kiosk must be valid sql-sources (either a table or a select in brackets)
+        that are already properly formatted for the target database driver.
+        :param from_kiosk: image information of the source kiosk
+        :param to_kiosk: image information of the target kiosk
+        :return:
+        """
+        sql = f"""select {KioskSQLDb.sql_safe_ident('from_kiosk')}.{KioskSQLDb.sql_safe_ident('uid')} 
+                    from {from_kiosk} from_kiosk 
+                    left outer join {to_kiosk} to_kiosk on from_kiosk.uid = to_kiosk.uid
+                    where to_kiosk.uid is null or to_kiosk.modified < from_kiosk.modified
+                    """
+        cur = KioskSQLDb.execute_return_cursor(sql)
+        c = 0
+        r = cur.fetchone()
+        delta = []
+        while r:
+            delta.append(r[0])
+            r = cur.fetchone()
+            c += 1
+            if c % 100 == 0:
+                self.console_log(f".", end="")
+
+        return delta
+
+    def pack_delta(self, target_dir, max_errors=1, max_zip_size=100000000, fake_it=False):
+        c = 0
+
+        def dot():
+            nonlocal c
+            c += 1
+            if c % 80 == 0:
+                self.console_log(".", flush=True)
+            else:
+                self.console_log(".", end="", flush=True)
+
+        cError = 0
+        try:
+            kfm = KioskFilesModel()
+            if not os.path.exists(target_dir):
+                os.mkdir(target_dir)
+        except BaseException as e:
+            logging.error(f"{self.__class__.__name__}.pack_delta: "
+                          f"Error when creating or accessing {target_dir}: {repr(e)}")
+            return False
+
+        self.console_log(f"Pack {len(self._delta)} files in {target_dir}:")
+        logging.debug(f"{self.__class__.__name__}.pack_delta: "
+                      f"Starting to pack {len(self._delta)} files in {target_dir}")
+        file_repos = FileRepository(self.cfg, )
+        zip_nr = 0
+        zip_size = 0
+        zip_file = None
+        try:
+            for f in self._delta:
+                dot()
+                filename = file_repos.get_file_repository_filename(f)
+                if filename and os.path.exists(filename):
+                    if not zip_file:
+                        zip_nr += 1
+                        zip_filename = f"kiosk_file_repos_{zip_nr}.zip"
+                        zip_size = 0
+                        zip_file = ZipFile(os.path.join(target_dir, zip_filename), 'w')
+                    if zip_file:
+                        try:
+                            filename_only = pathlib.PurePath(filename).name
+                            fr_path = pathlib.PurePath(filename).parent.name
+                            filename_in_zip = os.path.join(fr_path, filename_only)
+                            if not fake_it:
+                                zip_file.write(filename, arcname=filename_in_zip)
+                                try:
+                                    info = zip_file.getinfo(filename_in_zip.replace("\\", "/"))
+                                    zip_size += info.compress_size
+                                except BaseException as e:
+                                    logging.error(f"{self.__class__.__name__}.pack_delta: "
+                                                  f"Exception reflecting on {filename_in_zip}: {repr(e)}")
+                                    raise Exception("Zip operation failed")
+                            else:
+                                zip_size += int(kioskstdlib.get_file_size(filename) * 0.7)
+
+                            if zip_size > max_zip_size:
+                                zip_file.close()
+                                zip_file = None
+
+                        except BaseException as e:
+                            logging.error(f"{self.__class__.__name__}.pack_delta: Error zipping {filename}: {repr(e)}")
+                            raise Exception("Zip operation failed")
+
+                else:
+                    if not kfm.get_one("uid=%s", [f]) or not kfm.tags or "BROKEN_FILE" not in kfm.tags:
+                        cError += 1
+                        logging.error(f"{self.__class__.__name__}.pack_delta: Delta file '{f} not found'")
+                        if 0 < max_errors <= cError:
+                            raise Exception("Too many missing files")
+        except BaseException as e:
+            logging.error(f"{self.__class__.__name__}.pack_delta: Exception occurred: {repr(e)}")
+        finally:
+            if zip_file:
+                zip_file.close()
+
+        self.console_log(f"\nokay")
+        self.console_log(f"\n{c - cError} files packed, {cError} files missing.")
+        return True
+
+    def unpack(self, source_dir, file_repos_path="") -> bool:
+        c = 0
+
+        def dot():
+            nonlocal c
+            c += 1
+            if c % 80 == 0:
+                self.console_log(".", flush=True)
+            else:
+                self.console_log(".", end="", flush=True)
+
+        if not file_repos_path:
+            file_repos_path = self.cfg.get_file_repository(False)
+        if len(file_repos_path) < 5:
+            logging.error(f"{self.__class__.__name__}.unpack: "
+                          f"The path to the file repository has fewer than 5 characters. That does not seem healthy.")
+            return False
+        if not os.path.isdir(file_repos_path):
+            logging.error(f"{self.__class__.__name__}.unpack: "
+                          f"The path to the file repository ({file_repos_path}) does not point to a valid directory.")
+            return False
+
+        zip_files = kioskstdlib.find_files(source_dir, "kiosk_file_repos*.zip", order_desc=False)
+        self.console_log(f"Unpacking zip files from {source_dir} to {file_repos_path}.")
+        for zip_file_path in zip_files:
+            dot()
+            with ZipFile(zip_file_path, 'r') as zip_ref:
+                zip_ref.extractall(file_repos_path)
+
+        self.console_log(f"{c} zip files unpacked to {file_repos_path}.")
+        return True
---
+++ kiosk/tools/transfer_kiosk_cli.py
@@ -0,0 +1,175 @@
+import json
+import logging
+import os
+import sys
+import kioskstdlib
+import datetime
+from kioskconfig import KioskConfig
+from kiosktoolslib import get_kiosk_base_path_from_test_path, init_tool, interpret_all_params, check_required_options, \
+    split_param, get_kiosk_dir_param, init_dsd
+from kiosksqldb import KioskSQLDb
+from transferkiosk import TransferKiosk
+
+global_params = {"--kiosk-dir": "kiosk-dir",
+                 "-?": "?",
+                 "--help": "?",
+                 }
+
+command_params = {
+    "cat": {
+        "--target-dir": "target-dir"
+    },
+    "create-files-delta": {
+        "--target-catalog": "target-catalog",
+        "--source-catalog": "source-catalog",
+        "--delta-file": "delta-file"
+    },
+    "pack-delta": {
+        "--delta-file": "delta-file",
+        "--target-dir": "target-dir",
+        "--fake": "fake",
+        "--tolerated-errors": "tolerated-errors"
+    },
+    "unpack": {
+        "--source-dir": "source-dir",
+        "--file-repository": "file-repository"
+    }
+}
+
+required_params = {
+    "cat": ["target-dir"],
+    "create-files-delta": ["delta-file"],
+    "pack-delta": ["delta-file", "target-dir"],
+    "unpack": ["source-dir"]
+}
+
+options = {}
+command = ""
+
+
+def usage():
+    print("""
+    Usage of transfer_kiosk_cli.py:
+    ===================
+      transfer_kiosk_cli command [command-specific parameters][--kiosk-dir]    
+      <kiosk-dir> points to the local kiosk base directory on which to perform the command. 
+        If omitted transfer_kiosk expects to run in a subdirectory of kiosk and will try to find the kiosk root. 
+
+      commands and their parameters: 
+        cat --target-dir=target directory
+            creates information about the file repository of the source machine 
+            that can be used to run "create-files-delta" on the target machine             
+
+            target-dir: the directory in which the result will be stored. The result must be transferred to the source
+                        for the next step  
+        create-files-delta --delta-file=<delta file> 
+                           --target-catalog=<target catalog file> | --source_catalog=<source catalog file>
+            finds the difference between two file repositories and stores it in a delta file 
+ 
+            target-catalog: the catalog of the target server. Means that we are transferring from this server TO another  
+            source-catalog: the catalog of the source server. Means that we are transferring FROM that source server
+            delta-file: path and filename of the resulting delta information
+        pack-delta --delta-file=<delta file file> --target-dir=<target directory> [--fake] 
+                   [--tolerated-errors=number of tolerated errors]
+            packs all files that are listed in the delta file from the current kiosk into zip files 
+ 
+            delta-file: path and filename of the delta
+            target-dir: the directory where the zip files will be stored.
+            fake: if present, no actual files will be copied, only fake textfiles will be zipped. For testing purposes.
+            tolerated-errors: acceptable number of missing source files while packing 
+        unpack --source-dir=<dir with zips> [--file-repository=<path to the file repository>]
+            unpacks all the zip files in the source-dir into the file repository. Attention: This does not check again
+            if the files are actually newer! The tool trusts that the zip file is the result of the preceding steps.
+            
+            source-dir: The directory with the zip file
+            file-repository: Optional. The file repository path if you want to extract the files to a different place.
+    """)
+    sys.exit(0)
+
+
+def interpret_param(known_param, param):
+    new_option = global_params[known_param]
+
+    if new_option in ["target-dir", "td", "target-catalog", "source-catalog", "delta-file", "source-dir",
+                      "file-repository", "tolerated-errors"]:
+        rc = split_param(new_option, param)
+    else:
+        rc = {new_option: None}
+
+    return rc
+
+
+def cat():
+    if not transfer_kiosk.cat(options["target-dir"]):
+        usage()
+
+
+def create_files_delta():
+    target_catalog = kioskstdlib.try_get_dict_entry(options, "target-catalog", "")
+    source_catalog = kioskstdlib.try_get_dict_entry(options, "source-catalog", "")
+    if not transfer_kiosk.create_files_delta(target_catalog, source_catalog):
+        usage()
+    print(f"Saving files delta as {options['delta-file']} ... ", end="")
+    transfer_kiosk.delta2file(options["delta-file"])
+    print(f"okay.")
+
+
+def pack_delta():
+    print(f"Load files delta {options['delta-file']} ... ", end="")
+    transfer_kiosk.file2delta(options["delta-file"])
+    print(f"okay")
+    if "tolerated-errors" in options:
+        tolerated_errors = int(options["tolerated-errors"])
+    else:
+        tolerated_errors = 0
+    transfer_kiosk.pack_delta(options['target-dir'], fake_it=True if "fake" in options else False,
+                              max_errors=tolerated_errors)
+
+
+def unpack():
+    transfer_kiosk.unpack(options["source-dir"], options["file-repository"] if "file-repository" in options else "")
+
+
+def run_command():
+    if command == "cat":
+        cat()
+    if command == "create-files-delta":
+        create_files_delta()
+    if command == "pack-delta":
+        pack_delta()
+    if command == "unpack":
+        unpack()
+
+
+if __name__ == '__main__':
+    this_path = os.path.dirname(os.path.abspath(__file__))
+    kiosk_dir = get_kiosk_dir_param(2, {"--kiosk-dir"})
+    if kiosk_dir is None:
+        kiosk_dir = get_kiosk_base_path_from_test_path(this_path)
+    if not init_tool(os.path.join(kiosk_dir, "config", 'kiosk_config.yml')):
+        usage()
+
+    cfg = KioskConfig.get_config()
+    cfg.truncate_log()
+
+    if len(sys.argv) < 2:
+        logging.error(f"No command given.")
+        usage()
+
+    command = sys.argv[1]
+    if command not in command_params.keys():
+        logging.error(f"The command {command} is unknown.")
+        usage()
+
+    global_params.update(command_params[command])
+    options = interpret_all_params(2, global_params, interpret_param)
+    if options is None:
+        usage()
+
+    if command in required_params and not check_required_options(options, required_params[command]):
+        usage()
+
+    print(f"Kiosk Directory is {kiosk_dir}")
+    transfer_kiosk = TransferKiosk(cfg)
+    transfer_kiosk.enable_console = True
+    run_command()
