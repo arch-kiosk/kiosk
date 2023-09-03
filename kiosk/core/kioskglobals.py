8ddbd15f08c9e2fb9702c1f7847537b24c18f915
urapadmin <38838314+urapadmin@users.noreply.github.com>
2023-09-03 16:40:12 -0400

    new reporting capabilities for the urap inventory report

--- kiosk/core/kioskglobals.py
+++ kiosk/core/kioskglobals.py
@@ -18,9 +18,9 @@ from uic.uicstream import UICStream, UICKioskFile
 import datetime
 import threading
 
-kiosk_version = "1.5.7.1"
+kiosk_version = "1.5.9"
 kiosk_version_name = "queasy queries"
-kiosk_date = datetime.datetime(2023, 7, 28)
+kiosk_date = datetime.datetime(2023, 8, 24)
 debug = False
 development = {}
 
