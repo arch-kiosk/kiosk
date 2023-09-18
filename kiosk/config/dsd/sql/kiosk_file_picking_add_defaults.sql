bed2f510f426e889b5ece880c7993920e4878dcb
urapadmin <38838314+urapadmin@users.noreply.github.com>
2023-09-17 22:52:40 -0400

    corrected  standard file picking rules

--- kiosk/config/dsd/sql/kiosk_file_picking_add_defaults.sql
+++ kiosk/config/dsd/sql/kiosk_file_picking_add_defaults.sql
@@ -1,6 +1,9 @@
+delete from {{repl_file_picking_rules}} where uid = '5e730f79-daa9-43be-b613-93f03273eb76';
+delete from {{repl_file_picking_rules}} where uid = 'a25e7965-1083-4eca-ad66-015c30eae6f2';
+
 INSERT INTO {{repl_file_picking_rules}} (workstation_type, recording_group, "order", rule_type, operator, value,
-                                            resolution, disable_changes, misc, uid, created, modified, modified_by)
-select 'FileExportWorkstation',
+                                         resolution, disable_changes, misc, uid, created, modified, modified_by)
+select 'FileMakerWorkstation',
        'default',
        0,
        'contextuals',
@@ -16,11 +19,12 @@ select 'FileExportWorkstation',
 where not exists(select uid
                  from {{repl_file_picking_rules}}
                  where upper(recording_group) = 'DEFAULT'
+                   and upper(workstation_type) = 'FILEMAKERWORKSTATION'
                    and ("order" = 0 or upper("rule_type") = 'CONTEXTUALS'));
 
 INSERT INTO {{repl_file_picking_rules}} (workstation_type, recording_group, "order", rule_type, operator, value,
-                                            resolution, disable_changes, misc, uid, created, modified, modified_by)
-select 'FileExportWorkstation',
+                                         resolution, disable_changes, misc, uid, created, modified, modified_by)
+select 'FileMakerWorkstation',
        'default',
        10,
        'TAG',
@@ -33,5 +37,9 @@ select 'FileExportWorkstation',
        now(),
        now(),
        'sys'
-where not exists(select uid from {{repl_file_picking_rules}} where upper(recording_group) = 'DEFAULT'
-                   and upper("rule_type") = 'TAG' and "value" = 'map');
+where not exists(select uid
+                 from {{repl_file_picking_rules}}
+                 where upper(recording_group) = 'DEFAULT'
+                   and upper(workstation_type) = 'FILEMAKERWORKSTATION'
+                   and upper("rule_type") = 'TAG'
+                   and "value" = 'map');
