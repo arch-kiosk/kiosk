35703727d84b28fb7c8a94800469f562df55599b
urapadmin <38838314+urapadmin@users.noreply.github.com>
2024-01-15 17:52:33 -0500

    bumped up version

--- kiosk/core/kioskglobals.py
+++ kiosk/core/kioskglobals.py
@@ -19,9 +19,9 @@ from uic.uicstream import UICStream, UICKioskFile
 import datetime
 import threading
 
-kiosk_version = "1.5.18"
+kiosk_version = "1.5.20"
 kiosk_version_name = "vague views"
-kiosk_date = datetime.datetime(2024, 1, 6)
+kiosk_date = datetime.datetime(2024, 1, 15)
 debug = False
 development = {}
 
