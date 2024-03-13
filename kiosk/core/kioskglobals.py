54fa2230d396c101bf25dded041d6f9fb9cc69a2
urapadmin <38838314+urapadmin@users.noreply.github.com>
2024-03-13 17:45:36 +0100

    first version of Stratigraphic Studio

--- kiosk/core/kioskglobals.py
+++ kiosk/core/kioskglobals.py
@@ -19,9 +19,9 @@ from uic.uicstream import UICStream, UICKioskFile
 import datetime
 import threading
 
-kiosk_version = "1.5.24.3"
+kiosk_version = "1.5.26"
 kiosk_version_name = "vague views"
-kiosk_date = datetime.datetime(2024, 2, 24)
+kiosk_date = datetime.datetime(2024, 3, 12)
 debug = False
 development = {}
 
