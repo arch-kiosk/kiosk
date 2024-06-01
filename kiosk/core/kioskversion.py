4fb8df505938ddd1b1c1b437a4e830b8a884ae6b
urapadmin <38838314+urapadmin@users.noreply.github.com>
2024-06-01 15:42:23 -0400

    bumped up version

--- kiosk/core/kioskversion.py
+++ kiosk/core/kioskversion.py
@@ -1,5 +1,5 @@
 import datetime
 
-kiosk_version = "1.6"
+kiosk_version = "1.6.0.1"
 kiosk_version_name = "slippery search"
-kiosk_date = datetime.datetime(2024, 5, 28)
+kiosk_date = datetime.datetime(2024, 5, 29)
