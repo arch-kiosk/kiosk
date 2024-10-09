54f9d7c606db8af42211df371d34f0068da89117
urapadmin <38838314+urapadmin@users.noreply.github.com>
2024-10-09 19:37:50 -0400

    period uncertain for small finds on view and in structure

--- kiosk/core/kioskversion.py
+++ kiosk/core/kioskversion.py
@@ -1,5 +1,5 @@
 import datetime
 
-kiosk_version = "1.6.2.2"
+kiosk_version = "1.6.2.4"
 kiosk_version_name = "slippery search"
-kiosk_date = datetime.datetime(2024, 10, 8)
+kiosk_date = datetime.datetime(2024, 10, 9)
