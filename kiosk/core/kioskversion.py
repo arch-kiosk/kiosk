c7f8211157cc6b9f5ef318b237aa1da06260665f
urapadmin <38838314+urapadmin@users.noreply.github.com>
2024-10-16 16:43:53 -0400

    ui-component that shows selected image in gallery

--- kiosk/core/kioskversion.py
+++ kiosk/core/kioskversion.py
@@ -1,5 +1,5 @@
 import datetime
 
-kiosk_version = "1.6.2.4"
+kiosk_version = "1.6.2.5"
 kiosk_version_name = "slippery search"
-kiosk_date = datetime.datetime(2024, 10, 9)
+kiosk_date = datetime.datetime(2024, 10, 16)
