2d2687dc668af46a553df710b8b04084ce138948
urapadmin <38838314+urapadmin@users.noreply.github.com>
2025-04-02 22:24:38 +0200

    mouse bug in hm component fixed

--- kiosk/core/kioskversion.py
+++ kiosk/core/kioskversion.py
@@ -1,6 +1,6 @@
 import datetime
 
-kiosk_version = "1.6.28.1"
+kiosk_version = "1.6.28.2"
 kiosk_version_name = "tiddliest times"
-kiosk_date = datetime.datetime(2025,3,31)
+kiosk_date = datetime.datetime(2025,4,2)
 
