4ff06ac2cd23cf234ff6b601973ca4389c433a94
urapadmin <38838314+urapadmin@users.noreply.github.com>
2025-08-31 13:58:48 -0400

    bumped up version

--- kiosk/core/kioskversion.py
+++ kiosk/core/kioskversion.py
@@ -1,6 +1,6 @@
 import datetime
 
-kiosk_version = "1.7.18"
+kiosk_version = "1.7.18.2"
 kiosk_version_name = "tiddliest times"
-kiosk_date = datetime.datetime(2025,8,28)
+kiosk_date = datetime.datetime(2025,8,31)
 
