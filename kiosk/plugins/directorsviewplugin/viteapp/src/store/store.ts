5a547b18cbf869a6b2cfd4d049fa1984bcad6185
urapadmin <38838314+urapadmin@users.noreply.github.com>
2025-09-07 11:53:32 -0400

    made sass run again

--- kiosk/plugins/directorsviewplugin/viteapp/src/store/store.ts
+++
@@ -1,7 +0,0 @@
-import {createStore} from 'redux';
-// @ts-ignore
-import {reducer} from './reducer.ts';
-
-export const store = createStore(reducer,
-// @ts-ignore
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
