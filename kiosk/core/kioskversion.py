ad1f1e2d0fdc5a52ef551119754127ed18fbf3d8
urapadmin <38838314+urapadmin@users.noreply.github.com>
2026-01-25 11:00:19 -0500

    different cm views for su_ceramics and pottery projects

--- kiosk/core/kioskversion.py
+++ kiosk/core/kioskversion.py
@@ -1,7 +1,7 @@
 import datetime
 
-kiosk_version = "1.7.33"
+kiosk_version = "1.7.34"
 kiosk_version_name = "tiddliest times"
-kiosk_date = datetime.datetime(2026,1,22)
+kiosk_date = datetime.datetime(2026,1,25)
 
 
