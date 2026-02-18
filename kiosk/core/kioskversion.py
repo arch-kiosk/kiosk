c2088a10d40d320841cfeb1de68f6fbfd9e50705
urapadmin <38838314+urapadmin@users.noreply.github.com>
2026-02-18 20:55:54 +0100

    bucket sift ratio added

--- kiosk/core/kioskversion.py
+++ kiosk/core/kioskversion.py
@@ -1,7 +1,7 @@
 import datetime
 
-kiosk_version = "1.7.38.3"
+kiosk_version = "1.7.39.1"
 kiosk_version_name = "tiddliest times"
-kiosk_date = datetime.datetime(2026,2,15)
+kiosk_date = datetime.datetime(2026,2,18)
 
 
