7eca272440ef8e85dda2e17bb090d714b304273e
urapadmin <38838314+urapadmin@users.noreply.github.com>
2026-03-27 00:49:28 -0400

    latest changes for anc and the forum import

--- kiosk/core/kioskversion.py
+++ kiosk/core/kioskversion.py
@@ -1,6 +1,6 @@
 import datetime
 
-kiosk_version = "1.7.40"
+kiosk_version = "1.7.40.2"
 kiosk_version_name = "tiddliest times"
-kiosk_date = datetime.datetime(2026,3,19)
+kiosk_date = datetime.datetime(2026,3,27)
 
