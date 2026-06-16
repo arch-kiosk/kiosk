2ff4950f5bc5b3a8c2443576d11eb3c528dd557a
urapadmin <lutz_klein@brown.edu>
2026-06-16 16:45:33 -0400

    bumped up vesion

--- kiosk/core/kioskversion.py
+++ kiosk/core/kioskversion.py
@@ -1,5 +1,5 @@
 import datetime
 
-kiosk_version = "1.7.55"
+kiosk_version = "1.7.56"
 kiosk_version_name = "tiddliest times"
-kiosk_date = datetime.datetime(2026,6,11)
+kiosk_date = datetime.datetime(2026,6,13)
